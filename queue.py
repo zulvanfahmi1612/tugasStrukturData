14272d2e8686141581fbba502fb969ebbcc6f9f7
zulvanfahmi1612 <56451714+zulvanfahmi1612@users.noreply.github.com>
2020-05-30 00:21:55 +0700

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,17 @@
+def createQueue():
+    q=[]
+    return q
+
+def enqueue(q,data):
+    q.insert(0, data)
+    return q
+
+def dequeue(q):
+    data = q.pop()
+    return data
+
+def isEmpty(q):
+    return (q==[])
+
+def size(q):
+    return (len(q))
